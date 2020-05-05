41f9a271cb0faba4cd16c3350719f238499a171e
Brian Berg <bberg11@gmail.com>
2020-05-05 14:55:43 -0400

    Fix path to images directory

--- server/middleware/file.js
+++ server/middleware/file.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     if (isValid) {
       error = null;
     }
-    callback(error, 'backend/images');
+    callback(error, 'server/images');
   },
   filename: (request, file, callback) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
